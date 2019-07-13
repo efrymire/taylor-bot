0fd7a4f7d1a6f67c670c5979c53fd75b86ac479a
efrymire <efrymire@eleanors-mbp.home>
2019-07-13 11:14:47 -0400

    remove id

--- tweet_lyrics.js
+++ tweet_lyrics.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const request = require('request-promise')
 let { auth } = require('./utils')
 
-const createRequestOps = (tweet, id) => {
+const createRequestOps = (tweet) => {
 	const request_options = {
 		oauth: auth.twitter_oauth,
 		url: 'https://api.twitter.com/1.1/statuses/update.json?status=' + encodeURIComponent(tweet),
@@ -23,5 +23,5 @@ module.exports.tweet_lyrics = tweet_lyrics = () => {
 	// console.log(tweet)
 	console.log(tweet)
 	// const tweet = " I was riding shotgun with my hair undone in the front seat of his car, He's got a one-hand feel on the steering wheel, The other on my heart, I look around, turn the radio down, he says, 'baby is something wrong?' I say 'nothing, I was just thinking how we don't have a song,'  And he says,"
-	postResponse(createRequestOps(tweet, id));
+	postResponse(createRequestOps(tweet));
 }
